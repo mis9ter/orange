deb15d9b4f74b0abf5bf7853351fe468624046b4
mis9ter <kim.tigerj@gmail.com>
2021-06-27 14:48:49 +0900

    실행 파일 크기를 줄이기 위해 뭘 더 할까 보다보니 아이콘이 너무 큰걸로 썼더라. 작은걸로 교체.

--- agent/update/resource.h
+++ agent/update/resource.h
@@ -1,12 +1,14 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by update.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// update.rc에서 사용되고 있습니다.
+//
+#define IDI_ICON1                       101
 
-// 다음은 새 개체에 사용할 기본값입니다.
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
